879ebe4bc8a53ad08a70c4ecc827bf26b9931c92
ETU\r.maurouard <r.maurouard@rubika-edu.com>
2017-03-24 19:25:28 +0100

    no message

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour {
+
+    public static MenuManager instance;
+    public SceneTransition sceneTransition;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Play()
+    {
+        sceneTransition.PlayAnimation();
+        StartCoroutine(LoadSceneByIndex(1, 1));
+    }
+
+    public void ReturnToMainMenu()
+    {
+        sceneTransition.PlayAnimation();
+        StartCoroutine(LoadSceneByIndex(0, 1));
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+
+    IEnumerator LoadSceneByIndex(int index, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ Assets/Scripts/UI/SceneTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+public class SceneTransition : MonoBehaviour {
+    private Image panel;
+
+	void Start () {
+        panel = GetComponent<Image>();
+        panel.transform.DOScale(0, 0);
+        panel.DOFade(0, 0);
+        MenuManager.instance.sceneTransition = this;
+    }
+	
+    public void PlayAnimation()
+    {
+        panel.transform.DOScale(1, 0.5f);
+        panel.DOFade(1, 0.5f);
+    }
+}
